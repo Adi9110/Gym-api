198f4698628c8b2cd87e45f4dd0b8ff975f1ddd7
Adi Reddy <Adireddyy19@gmail.com>
2025-04-30 12:38:22 +0530

    entity craeted

--- Gym-api-1/src/main/java/com/gym/ServletInitializer.java
+++ Gym-api-1/src/main/java/com/gym/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.adi;
+package com.gym;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
---
+++ Gym-api-1/src/main/java/com/gym/repo/UserRepo.java
@@ -0,0 +1,5 @@
+package com.gym.repo;
+
+public class UserRepo {
+
+}
