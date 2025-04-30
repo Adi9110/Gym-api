8a4e586460c2622b0df189f4ae4174b5e921d0a8
Adi Reddy <Adireddyy19@gmail.com>
2025-04-30 12:34:55 +0530

    initial commit

---
+++ Gym-api-1/src/main/java/com/adi/GymApi1Application.java
@@ -0,0 +1,13 @@
+package com.adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApi1Application.class, args);
+	}
+
+}
---
+++ Gym-api-1/src/test/java/com/adi/GymApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
